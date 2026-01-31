d2bad443f00ece003e66eaa595e2c86065cab7a0
pzaro <pzaro2010@gmail.com>
2026-01-31 07:36:37 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'pharmacy-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js?v=6',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Αν υπάρχει στη μνήμη, το δίνουμε. Αλλιώς το ζητάμε από το ίντερνετ.
+        return response || fetch(event.request);
+      })
+  );
+});
